bfd3bb06d39d332407b8566bbaf272cbe8d215dc
Scott Golaszewski <sgola37@gmail.com>
2015-01-23 21:10:04 -0700

    Add some spheres and mix up the z orbit off-axis a little bit.

--- js/main.js
+++ js/main.js
@@ -10,12 +10,17 @@ jQuery(function() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.shadowMapEnabled = true;
 
+	// Set a bunch of sphere sizes to pick from
 	var sphereGeometries = [
 		new THREE.SphereGeometry(1, 32, 32),
 		new THREE.SphereGeometry(2, 32, 32),
-		new THREE.SphereGeometry(3, 32, 32)
+		new THREE.SphereGeometry(3, 32, 32),
+		new THREE.SphereGeometry(4, 32, 32),
+		new THREE.SphereGeometry(5, 32, 32),
+		new THREE.SphereGeometry(6, 32, 32)
 	];
 
+	// The surfaces
 	var sphereMaterials = [
 		new THREE.MeshLambertMaterial({ color: 0x257E78 }),
 		new THREE.MeshLambertMaterial({ color: 0x40B8AF }),
@@ -25,7 +30,7 @@ jQuery(function() {
 		new THREE.MeshLambertMaterial({ color: 0xFFFFFF })
 	];
 
-	var numberOfSpheres = 10;
+	var numberOfSpheres = 37;
 	var spheres = [];
 
 	for (var i = 0; i < numberOfSpheres; i++) {
@@ -33,19 +38,19 @@ jQuery(function() {
 		var randomMaterial = sphereMaterials[Math.floor(Math.random() * sphereMaterials.length)];
 		var sphere = new THREE.Mesh(randomGeometry, randomMaterial);
 
-		sphere.position.x = Math.floor(Math.random() * 10) * Math.round(Math.random()) * 2 -1;
-		sphere.position.y = Math.floor(Math.random() * 10) * Math.round(Math.random()) * 2 -1;
-		sphere.position.z = Math.floor(Math.random() * 10) * Math.round(Math.random()) * 2 -1;
+		sphere.position.x = Math.floor(Math.random() * 10) * (Math.round(Math.random()) * 2 - 1);
+		sphere.position.y = Math.floor(Math.random() * 10) * (Math.round(Math.random()) * 2 - 1);
+		sphere.position.z = Math.floor(Math.random() * 10) * (Math.round(Math.random()) * 2 - 1);
 		sphere.castShadow = true;
 		sphere.receiveShadow = true;
 
 		sphere.step = 0;
-		sphere.stepIncrement = Math.random() / 10.0;
+		sphere.stepIncrement = Math.random() / 20.0;
 
 		sphere.orbitOffset = {
-			x: Math.floor(Math.random() * 10),
-			y: Math.floor(Math.random() * 10),
-			z: Math.floor(Math.random() * 10)
+			x: Math.floor(Math.random() * 20) + 10,
+			y: Math.floor(Math.random() * 20) + 10,
+			z: Math.floor(Math.random() * 20)
 		}
 
 		// Add sphere to the spheres array
@@ -54,13 +59,17 @@ jQuery(function() {
 		scene.add(sphere);
 	}
 
-	camera.position.x = -30;
-	camera.position.y = 40;
-	camera.position.z = 30;
+	camera.position.x = -50;
+	camera.position.y = 60;
+	camera.position.z = 50;
 	camera.lookAt(scene.position);
 
-	var spotLight = new THREE.SpotLight( 0xffffff );
-	spotLight.position.set( -40, 60, -10 );
+	// Add some ambient light
+	// var ambientLight = new THREE.AmbientLight( 0x202020 );
+	// scene.add(ambientLight);
+
+	var spotLight = new THREE.SpotLight( 0xFFFFFF );
+	spotLight.position.set( -100, 100, -100 );
 	spotLight.castShadow = true;
 	scene.add( spotLight );
 
@@ -70,24 +79,25 @@ jQuery(function() {
 
 	function renderScene() {
 
-	  var center = {
-	  	x: 0,
-	  	y: 0,
-	  	z: 0
-	  };
-
 	  spheres.forEach(function(sphere) {
 	  	sphere.step += sphere.stepIncrement;
+
+	  	// Have the first sphere orbit the origin
+	  	var center = {
+	  		x: 0,
+	  		y: 0,
+	  		z: 0
+	  	};
 
 	  	if (typeof center.x !== 'undefined') {
 				sphere.position.x = center.x + (sphere.orbitOffset.x * (Math.cos(sphere.step)));
 				sphere.position.y = center.y + (sphere.orbitOffset.y * (Math.sin(sphere.step)));
-				sphere.position.z = center.z + (sphere.orbitOffset.z * (Math.cos(sphere.step)));
+				sphere.position.z = center.z + (sphere.orbitOffset.z * (Math.sin(sphere.step))) * 5;
 	  	}
 
-	  	center.x = sphere.position.x;
-	  	center.y = sphere.position.y;
-	  	center.z = sphere.position.z;
+	  	// Pick another random sphere to orbit
+	  	center = spheres[Math.floor(Math.random() * spheres.length)].position;
+
 	  });
 
 
